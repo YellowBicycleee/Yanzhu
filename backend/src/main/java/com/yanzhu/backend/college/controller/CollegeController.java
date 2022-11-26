d82e04031963bd856fdf9dd85798db89c9a95458
wangjiancheng <wangjianchengcs@163.com>
2022-11-26 21:45:12 +0800

    考研信息增加----院校线

--- backend/src/main/java/com/yanzhu/backend/college/controller/CollegeController.java
+++ backend/src/main/java/com/yanzhu/backend/college/controller/CollegeController.java
@@ -3,16 +3,20 @@ package com.yanzhu.backend.college.controller;
 import com.alibaba.fastjson.JSON;
 import com.yanzhu.backend.college.entity.College;
 import com.yanzhu.backend.college.service.ICollegeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.yanzhu.backend.util.Constant.DEVELOP_ORIGIN;
+import static com.yanzhu.backend.util.Constant.PRODUCE_ORIGIN;
+
+@Api(value = "学校院系信息获取接口", tags = "学校院系信息获取接口说明")
+@CrossOrigin(origins={DEVELOP_ORIGIN, PRODUCE_ORIGIN}, allowCredentials="true")
 @RestController
 @RequestMapping("/college")
 public class CollegeController {
@@ -20,6 +24,7 @@ public class CollegeController {
     private ICollegeService collegeService;
 
     @GetMapping("all")
+    @ApiOperation(value = "返回所有学校")
     public String queryAllCollege(){
         Map<String, Object> map = new HashMap<String, Object>();
         try{
@@ -44,6 +49,7 @@ public class CollegeController {
         return JSON.toJSONString(map);
     }
     @GetMapping("/{province}")
+    @ApiOperation(value = "按省份返回学校")
     public String queryCollegeByCity(@PathVariable("province") String city) {
         Map<String, Object> map = new HashMap<String, Object>();
         try{
