670f271f65e0c81ab756c54ee2268dfe5fd6fac7
wangjiancheng <wangjianchengcs@163.com>
2022-12-10 01:15:28 +0800

    修改测试类

--- backend/src/test/java/com/yanzhu/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/yanzhu/backend/BackendApplicationTests.java
@@ -12,9 +12,30 @@ import com.yanzhu.backend.postgraduateTest.service.IPostgraduateTestServiceTest;
 import com.yanzhu.backend.user.controller.UserControllerTest;
 import com.yanzhu.backend.user.service.IUserServiceTest;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
 import org.junit.runner.RunWith;
+import org.junit.runner.notification.Failure;
 import org.junit.runners.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {ContestControllerTest.class,
+		IContestServiceTest.class,
+		CourseControllerTest.class,
+		ICourseServiceTest.class,
+		PostgraduateRecommendationTest.class,
+		IPostgraduateRecommendationServiceTest.class,
+		PostgraduateTestControllerTest.class,
+		IPostgraduateTestServiceTest.class,
+		UserControllerTest.class,
+		IUserServiceTest.class})
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
 
 //@SpringBootTest
 //class BackendApplicationTests {
@@ -24,20 +45,29 @@ import org.springframework.boot.test.context.SpringBootTest;
 //	}
 //
 //}
-@RunWith(Suite.class)
-@Suite.SuiteClasses({
-		ContestControllerTest.class,
-		IContestServiceTest.class,
-		CourseControllerTest.class,
-		ICourseServiceTest.class,
-		PostgraduateRecommendationTest.class,
-		IPostgraduateRecommendationServiceTest.class,
-		PostgraduateTestControllerTest.class,
-		IPostgraduateTestServiceTest.class,
-		UserControllerTest.class,
-		IUserServiceTest.class
-})
-public class BackendApplicationTests {
-	// This class remains empty, it is used only as a holder for the above annotations
-}
+//@RunWith(Suite.class)
+//@Suite.SuiteClasses({
+//		ContestControllerTest.class,
+//		IContestServiceTest.class,
+//		CourseControllerTest.class,
+//		ICourseServiceTest.class,
+//		PostgraduateRecommendationTest.class,
+//		IPostgraduateRecommendationServiceTest.class,
+//		PostgraduateTestControllerTest.class,
+//		IPostgraduateTestServiceTest.class,
+//		UserControllerTest.class,
+//		IUserServiceTest.class
+//})
+//class TestClass{}
+//public class BackendApplicationTests {
+//	// This class remains empty, it is used only as a holder for the above annotations
+//	public static void main(String[] args) {
+//		Result result = JUnitCore.runClasses(TestClass.class);
+//
+//		for (Failure failure : result.getFailures()) {
+//			System.out.println(failure.toString());
+//		}
+//		System.out.println(result.wasSuccessful());
+//	}
+//}
 
