5e0fc6638f8f2156ec6f95ecaf258e6ae95e7c6d
wangjiancheng <wangjianchengcs@163.com>
2022-12-10 17:26:17 +0800

    修改跨域问题

--- backend/src/main/java/com/yanzhu/backend/util/Constant.java
+++ backend/src/main/java/com/yanzhu/backend/util/Constant.java
@@ -1,7 +1,7 @@
 package com.yanzhu.backend.util;
 
 public class Constant {
-    public static final String PRODUCE_ORIGIN = "http://152.136.103.209";
+    public static final String PRODUCE_ORIGIN = "http://152.136.103.209:8080";
     public static final String DEVELOP_ORIGIN = "http://localhost:8080";
     public static final String VERSION = "v0";
 }
