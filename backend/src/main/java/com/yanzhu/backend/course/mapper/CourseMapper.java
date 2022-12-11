b8359abdecbd50d8cbfcd02e7524a5f65a5be9a0
GDSnyWang <117516150+GDSnyWang@users.noreply.github.com>
2022-12-11 23:11:32 +0800

    Delete backend directory

--- backend/src/main/java/com/yanzhu/backend/course/mapper/CourseMapper.java
+++
@@ -1,9 +0,0 @@
-package com.yanzhu.backend.course.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.yanzhu.backend.course.entity.Course;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface CourseMapper extends BaseMapper<Course> {
-}
