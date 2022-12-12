53da821f191a861814d2e01ee5a9015c236788c9
GDSnyWang <117516150+GDSnyWang@users.noreply.github.com>
2022-12-12 16:56:13 +0800

    Delete backend directory

--- backend/src/main/java/com/yanzhu/backend/postgraduateRecommendation/mapper/PostgraduateRecommendationMapper.java
+++
@@ -1,9 +0,0 @@
-package com.yanzhu.backend.postgraduateRecommendation.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.yanzhu.backend.postgraduateRecommendation.entity.PostgraduateRecommendation;
-import org.mapstruct.Mapper;
-
-@Mapper
-public interface PostgraduateRecommendationMapper extends BaseMapper<PostgraduateRecommendation> {
-}
